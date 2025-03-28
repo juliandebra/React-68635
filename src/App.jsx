571c190a0ae42a67a10c6528f3d32c986ecacece
juliandebra <julian.debra25@gmail.com>
2025-03-27 21:06:05 -0300

    Clase 2: Creación de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Ejemplo from './Components/Ejemplo'
+
+function App() {
+  return (
+    <>
+      <h1>Hola coders!</h1>
+      <h1>Volvemos a las 20:40!</h1>
+      <Ejemplo />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Ejemplo.jsx
@@ -0,0 +1,9 @@
+const Ejemplo = () => {
+  return (
+    <div>
+      <h1>Este es mi componente de ejemplo</h1>
+    </div>
+  )
+}
+
+export default Ejemplo
