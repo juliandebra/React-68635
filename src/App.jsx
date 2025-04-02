aecff7990a8b3708bcb2fa2e0cf39a0f91fba2a6
juliandebra <julian.debra25@gmail.com>
2025-04-01 22:25:21 -0300

    Clase 3: Sugar Syntax, Polyfills, Webpack, Style y Eventos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,63 @@
 import './App.css'
 import Ejemplo from './Components/Ejemplo'
+import Estudiante from './Components/Estudiante'
+import Estudi from './Components/Estudiante'
+const titleStyle = {
+  backgroundColor: 'green',
+  color: 'black',
+  fontSize: '20px',
+  padding: 100,
+}
 
 function App() {
+  //Parte lógica sintaxis Js
+
+  const auto = {
+    name: '',
+    marca: 'Fiat',
+    modelo: '600',
+    status: 'ok',
+    color: 'purple',
+  }
+  const arr = ['Flor', 'Oclivar', 'Analia']
+
+  // const marca = auto.marca
+  // const modelo = auto.modelo
+  // const color = auto.color
+
+  const { marca, modelo, color } = auto
+  console.log(marca, modelo, color)
+
+  // const name1 = arr[0]
+  // const name2 = arr[1]
+  // const name3 = arr[2]
+
+  const [name1, name2, name3] = arr
+  console.log('flor: ', name1, ' Oclivar: ', name2, ' Analia: ', name3)
+
+  const saludo = () => {
+    console.log('Hola que tal?')
+  }
+
   return (
-    <>
+    <div>
+      {/* Sintaxis HTML */}
+      <h1 style={titleStyle}>Volvemos a las 20:17!!</h1>
       <h1>Hola coders!</h1>
-      <h1>Volvemos a las 20:40!</h1>
+      <h2
+        className='titulo2'
+        style={{
+          backgroundColor: color == 'red' ? 'red' : 'white',
+          color: color === 'red' ? 'white' : 'black',
+        }}
+      >
+        {marca} - {modelo}
+      </h2>
+      <button onClick={saludo}>Saludar</button>
+      <h3>{color}</h3>
       <Ejemplo />
-    </>
+      <Estudiante />
+    </div>
   )
 }
 
---
+++ src/Components/Estudiante.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Estudiante = () => {
+  return <div>Estudiante</div>
+}
+
+export default Estudiante
